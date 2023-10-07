9e980e2b51a653a1b61ab1f3f16c1e02091cd101
beatrizon <beatriz.sousa2@estudante.ifb.edu.br>
2023-10-07 18:45:02 -0300

    fist commit

--- notifica_app/lib/main.dart
+++ notifica_app/lib/main.dart
@@ -37,7 +37,7 @@ final GoRouter _router = GoRouter(routes: <RouteBase>[
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+// isso é um comentário
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
